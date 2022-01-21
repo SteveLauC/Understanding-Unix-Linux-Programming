fa6bf847220975b9fda480cd90751ae10cb87075
Steve Lau <stevelauc@outlook.com>
2022-01-21 22:38:15 +0800

    finish who1

--- Ch2/01.who/who1-rs/target/debug/build/num-traits-ad2186e8b1fe24bf/build_script_build-ad2186e8b1fe24bf.d
+++
@@ -1,5 +0,0 @@
-/home/steve/Documents/workspace/Understanding-Unix-Linux-Programming/Ch2/01.who/who1-rs/target/debug/build/num-traits-ad2186e8b1fe24bf/build_script_build-ad2186e8b1fe24bf: /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/num-traits-0.2.14/build.rs
-
-/home/steve/Documents/workspace/Understanding-Unix-Linux-Programming/Ch2/01.who/who1-rs/target/debug/build/num-traits-ad2186e8b1fe24bf/build_script_build-ad2186e8b1fe24bf.d: /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/num-traits-0.2.14/build.rs
-
-/home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/num-traits-0.2.14/build.rs:
