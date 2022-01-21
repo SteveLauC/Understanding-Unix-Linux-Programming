f5bac8bb4f8507ce587031278eaf01161d4937a2
Steve Lau <stevelauc@outlook.com>
2022-01-21 22:34:33 +0800

    finish who1

---
+++ Ch2/01.who/who1-rs/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::Read;
+use std::time::{Duration, UNIX_EPOCH};
+use std::{ffi, mem, slice};
+
+use chrono::{DateTime, Local};
+use libc::utmpx;
+
+fn show_info(ut_buf_p: &utmpx) {
+    unsafe {
+        print!("{:?}", ffi::CStr::from_ptr(ut_buf_p.ut_user.as_ptr()));
+        print!(" ");
+        print!("{:?}", ffi::CStr::from_ptr(ut_buf_p.ut_line.as_ptr()));
+        print!(" ");
+    }
+    let date: DateTime<Local> = DateTime::from(
+        UNIX_EPOCH + Duration::from_secs(u64::try_from(ut_buf_p.ut_tv.tv_sec).unwrap()),
+    );
+    let date_str: String = date.format("%a %b %e %H:%M:%S %Y").to_string();
+    print!("{}", date_str);
+    print!(" ");
+    unsafe {
+        print!("{:?}", ffi::CStr::from_ptr(ut_buf_p.ut_host.as_ptr()));
+    }
+    println!();
+}
+
+fn main() {
+    let mut utmp_file: File = File::open("/var/run/utmp").unwrap();
+    let struct_size: usize = mem::size_of::<utmpx>();
+    unsafe {
+        let mut current_record: utmpx = mem::zeroed();
+        let buffer: &mut [u8] =
+            slice::from_raw_parts_mut(&mut current_record as *mut utmpx as *mut u8, struct_size);
+        while utmp_file.read_exact(buffer).is_ok() {
+            show_info(&current_record);
+        }
+    }
+}
---
+++ Ch2/01.who/who1-rs/target/debug/build/libc-8eb3ab4692b94199/build_script_build-8eb3ab4692b94199.d
@@ -0,0 +1,5 @@
+/home/steve/Documents/workspace/Understanding-Unix-Linux-Programming/Ch2/01.who/who1-rs/target/debug/build/libc-8eb3ab4692b94199/build_script_build-8eb3ab4692b94199: /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/libc-0.2.113/build.rs
+
+/home/steve/Documents/workspace/Understanding-Unix-Linux-Programming/Ch2/01.who/who1-rs/target/debug/build/libc-8eb3ab4692b94199/build_script_build-8eb3ab4692b94199.d: /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/libc-0.2.113/build.rs
+
+/home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/libc-0.2.113/build.rs:
