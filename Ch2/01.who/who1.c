3e06109f375c3fcb019550b641e3d848e1877c84
Steve Lau <stevelauc@outlook.com>
2022-01-22 18:30:31 +0800

    format c code using GNU indent, and I don't like its style:(

--- Ch2/01.who/who1.c
+++ Ch2/01.who/who1.c
@@ -15,29 +15,35 @@
 /*
  * display contents of the utmp struct in human readable format
 */
-void show_info(struct utmp * ut_buf_p){
-    printf("%-8.8s", ut_buf_p->ut_user);
-    printf(" ");
-    printf("%-8.8s", ut_buf_p->ut_line);
-    printf(" ");
-    long sec = (long)ut_buf_p->ut_tv.tv_sec;
-    printf("%s", ctime(&sec));
-    printf("(%s)", ut_buf_p->ut_host);
-    printf("\n");
+void
+show_info (struct utmp *ut_buf_p)
+{
+  printf ("%-8.8s", ut_buf_p->ut_user);
+  printf (" ");
+  printf ("%-8.8s", ut_buf_p->ut_line);
+  printf (" ");
+  long sec = (long) ut_buf_p->ut_tv.tv_sec;
+  printf ("%s", ctime (&sec));
+  printf ("(%s)", ut_buf_p->ut_host);
+  printf ("\n");
 }
 
-int main(){
-    struct utmp current_record; 
-    int utmpfd;
-    size_t rec_len = sizeof(struct utmp);
+int
+main ()
+{
+  struct utmp current_record;
+  int utmpfd;
+  size_t rec_len = sizeof (struct utmp);
 
-    if (( utmpfd = open("/var/run/utmp", O_RDONLY)) == -1){
-        perror(UTMP_FILE);
-        exit(-1);
+  if ((utmpfd = open ("/var/run/utmp", O_RDONLY)) == -1)
+    {
+      perror (UTMP_FILE);
+      exit (-1);
     }
-    while ((read(utmpfd, &current_record, rec_len) == rec_len)){
-        show_info(&current_record);
+  while ((read (utmpfd, &current_record, rec_len) == rec_len))
+    {
+      show_info (&current_record);
     }
-    close(utmpfd);
-    return 0;
+  close (utmpfd);
+  return 0;
 }
