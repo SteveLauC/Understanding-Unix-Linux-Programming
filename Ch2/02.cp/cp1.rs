0f2e02220a0ea2d08cf76b1f6a56548c618e11e9
Steve Lau <stevelauc@outlook.com>
2022-01-23 17:32:31 +0800

    finish cp1.rs

--- Ch2/02.cp/cp1.rs
+++ Ch2/02.cp/cp1.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::fs::{File, OpenOptions};
+use std::fs::{self, File, OpenOptions};
 use std::io::{Read, Result, Write};
 use std::os::unix::fs::OpenOptionsExt;
 use std::process;
@@ -16,6 +16,12 @@ fn main() {
     if av.len() != 3 {
         eprintln!("usage: {} source destination", av[0].as_str());
         process::exit(-1);
+    }
+
+    // read(2) can NOT read from a directory, however std::io::Read::read can do this.
+    // so I manually check it here.
+    if fs::metadata(av[1].as_str()).unwrap().is_dir() {
+        oops("is a directory", av[1].as_str());
     }
 
     let mut in_file: File = match File::open(av[1].as_str()) {
@@ -38,26 +44,25 @@ fn main() {
         // handle EOF
         if n_chars == 0 {
             break;
-        } 
- 
-		let write_res: Result<usize> = out_file.write(
-			if n_chars <  BUFFERSIZE {
-				buf.chunks(n_chars).next().unwrap()
-			}else{
-				&buf
-			}
-		);
+        }
+
+        let write_res: Result<usize> = out_file.write(if n_chars < BUFFERSIZE {
+			// The remaining size of file(av[1]) contents is less than BUFFERSIZE
+			// chunk it to get a smaller slice, whose length is same as the remaining size
+            buf.chunks(n_chars).next().unwrap()
+        } else {
+            &buf
+        });
         match write_res {
-             Ok(n) => {
-                 if n != n_chars {
-                     // println!("debug: write {} bytes, read {} bytes", n, n_chars);
-                     oops(av[2].as_str(), "Write error to ");
-                 }
-              }
-             Err(msg) => {
-                  oops(msg.to_string().as_str(), av[2].as_str());
-             }
-         }
+            Ok(n) => {
+                if n != n_chars {
+                    oops(av[2].as_str(), "Write error to ");
+                }
+            }
+            Err(msg) => {
+                oops(msg.to_string().as_str(), av[2].as_str());
+            }
+        }
     }
 }
 
