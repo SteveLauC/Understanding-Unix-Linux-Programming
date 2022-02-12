6b50ac91b0f6ec0c645a686da34711a1ba7a27a8
Steve Lau <stevelauc@outlook.com>
2022-02-12 19:49:39 +0800

    finish listchars

---
+++ Ch5/02.listchars/listchars.c
@@ -0,0 +1,16 @@
+/*
+ * listchars.c
+ * purpose: list indicidually all the chars seen on input
+ * output: char and ascii code, one pair pere line
+ * input: stdin, until the letter Q
+ * notes: useful to show that buffering/editing exists
+*/
+
+#include <stdio.h>
+
+int main() {
+    int c, n = 0;
+    while ((c = getchar()) != 'Q') {
+        printf("char %3d is %c code %d\n", n++, c, c);
+    }
+}
---
+++ Ch5/02.listchars/listchars.rs
@@ -0,0 +1,21 @@
+use std::io::{Read, stdin};
+
+fn main() {
+    // for (i, c) in stdin().bytes().map(|x| x.expect("cannot read char from stdin")).enumerate() {
+    //     if c != b'Q' {
+    //         println!("char {:3} is {} code {}", i, c, c);
+    //     }else{
+    //         break;
+    //     }
+    // } 
+
+    stdin().bytes().enumerate().for_each(|(i, item)|{
+        if let Ok(c) = item {
+            if c == b'Q' {
+                std::process::exit(0);
+            }else{
+                println!("char {:3} is {} code {}", i, c, c);
+            }
+        }
+    });
+}
