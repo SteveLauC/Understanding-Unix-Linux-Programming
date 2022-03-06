7457bba4ee04189d4d7c72fe2197b5deba8bb0b7
Steve Lau <stevelauc@outlook.com>
2022-03-06 23:08:15 +0800

    rewrite setecho-rs using crate termios

--- Ch5/04.setecho/setecho-rs/src/main.rs
+++ Ch5/04.setecho/setecho-rs/src/main.rs
@@ -1,41 +1,32 @@
 use std::env;
 use std::process;
 
-extern crate libc;
-use libc::{termios, tcgetattr, tcsetattr, ECHO, TCSANOW};
+use termios::os::linux::{ECHO, TCSANOW};
+use termios::Termios;
 
 fn main() {
-    let mut buf: termios = termios{
-        c_iflag: 0,
-        c_oflag: 0,
-        c_cflag: 0,
-        c_lflag: 0,
-        c_line: 0,
-        c_cc: [0;32],
-        c_ispeed: 0,
-        c_ospeed: 0,
-    };
-
     let av: Vec<String> = env::args().collect();
-    if av.len()==1 {
+    if av.len() == 1 {
         eprintln!("usage: setecho [y|n]");
         process::exit(1);
     }
 
-
-    if -1 == unsafe{tcgetattr(0, &mut buf as *mut termios)} {
-        eprintln!("Can not fetch attribute for Stdin");
-        std::process::exit(1);
-    }
+    let mut buf: Termios = match Termios::from_fd(0) {
+        Ok(s) => s,
+        Err(msg) => {
+            eprintln!("can not fetch terminal driver configuration: {}", msg);
+            process::exit(1);
+        }
+    };
 
     if av[1].starts_with('y') {
         buf.c_lflag |= ECHO;
-    }else{
+    } else {
         buf.c_lflag &= !ECHO;
     }
-    
-    if -1 == unsafe{tcsetattr(0, TCSANOW, &buf)} {
-        eprintln!("Can not send modification back");
+
+    if termios::tcsetattr(0, TCSANOW, &buf).is_err() {
+        eprintln!("cannot send the modification back to the kernel");
         process::exit(1);
     }
 }
