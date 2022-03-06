50a74f234108680df1683157e1a1addbbf88eae9
Steve Lau <stevelauc@outlook.com>
2022-03-06 22:39:37 +0800

    rewrite echostate-rs using crate termios

--- Ch5/03.echostate/echostate-rs/target/debug/deps/termios-185b3298518e092b.d
+++
@@ -1,8 +0,0 @@
-/home/steve/Documents/workspace/Understanding-Unix-Linux-Programming/Ch5/03.echostate/echostate-rs/target/debug/deps/termios-185b3298518e092b.rmeta: /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/lib.rs /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/ffi.rs /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/os/mod.rs /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/os/linux.rs
-
-/home/steve/Documents/workspace/Understanding-Unix-Linux-Programming/Ch5/03.echostate/echostate-rs/target/debug/deps/termios-185b3298518e092b.d: /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/lib.rs /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/ffi.rs /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/os/mod.rs /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/os/linux.rs
-
-/home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/lib.rs:
-/home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/ffi.rs:
-/home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/os/mod.rs:
-/home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/termios-0.3.3/src/os/linux.rs:
