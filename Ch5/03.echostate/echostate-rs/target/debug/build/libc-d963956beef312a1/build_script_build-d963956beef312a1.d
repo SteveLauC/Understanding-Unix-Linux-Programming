a3cc7d47eca3e74a5b9fe18af31d94d9c4eaec7f
Steve Lau <stevelauc@outlook.com>
2022-03-06 22:38:11 +0800

    rewrite echostate-rs using crate termios

--- Ch5/03.echostate/echostate-rs/target/debug/build/libc-d963956beef312a1/build_script_build-d963956beef312a1.d
+++
@@ -1,5 +0,0 @@
-/home/steve/Documents/workspace/Understanding-Unix-Linux-Programming/Ch5/03.echostate/echostate-rs/target/debug/build/libc-d963956beef312a1/build_script_build-d963956beef312a1: /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/libc-0.2.119/build.rs
-
-/home/steve/Documents/workspace/Understanding-Unix-Linux-Programming/Ch5/03.echostate/echostate-rs/target/debug/build/libc-d963956beef312a1/build_script_build-d963956beef312a1.d: /home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/libc-0.2.119/build.rs
-
-/home/steve/.cargo/registry/src/mirrors.ustc.edu.cn-61ef6e0cd06fb9b8/libc-0.2.119/build.rs:
