896f4c31cfb02f7e77f41d36143e75c768ce13cf
Steve Lau <stevelauc@outlook.com>
2022-03-08 15:06:26 +0800

    finish play_again2

--- Ch6/02.play_again/play_again2-rs/src/main.rs
+++ Ch6/02.play_again/play_again2-rs/src/main.rs
@@ -1,24 +1,21 @@
-use std::process;
 use std::io::{self, stdin, Read, Write};
-use termios::{self, Termios, ECHO, ICANON, VMIN, TCSANOW, tcsetattr};
+use termios::{self, tcsetattr, Termios, ECHO, ICANON, TCSANOW, VMIN};
 
-enum Answer{
+enum Answer {
     Yes,
     No,
 }
 
-
 fn get_response() -> Answer {
     print!("Do you want another transaction(y/n)");
-    io::stdout().flush().unwrap(); 
+    io::stdout().flush().unwrap();
 
-    
     for n in stdin().bytes().map(|x| x.unwrap()) {
         if n == 'y' as u32 as u8 || n == 'Y' as u32 as u8 {
             return Answer::Yes;
         } else if n == 'n' as u32 as u8 || n == 'N' as u32 as u8 {
             return Answer::No;
-        }  
+        }
     }
 
     unreachable!()
@@ -37,11 +34,11 @@ fn set_cr_noecho_mode() {
 fn main() {
     let orig_mode: Termios = Termios::from_fd(0).expect("cannot get the orig mode");
     set_cr_noecho_mode();
-    match get_resonse() {
+    match get_response() {
         Answer::No => {
             tcsetattr(0, TCSANOW, &orig_mode).expect("cannot restore the original mode");
             std::process::exit(1);
-        },
+        }
         Answer::Yes => tcsetattr(0, TCSANOW, &orig_mode).expect("cannot restore the original mode"),
     }
 }
