2672f5faf4ef6555a5392e33cdc46e1bb7f4bb10
Steve Lau <stevelauc@outlook.com>
2022-03-26 15:56:44 +0800

    format rust code

--- Ch8/03.forkdemo/forkdemo1-rs/src/main.rs
+++ Ch8/03.forkdemo/forkdemo1-rs/src/main.rs
@@ -1,20 +1,19 @@
-use std::process;
 use nix::unistd::{fork, ForkResult};
-
+use std::process;
 
 fn main() {
     println!("Before: my pid is {}", process::id());
 
-    match unsafe{fork()} {
+    match unsafe { fork() } {
         Ok(ForkResult::Parent { child }) => {
             println!("After: my pid is {}, fork() said {}", process::id(), child);
-        },
+        }
 
         Ok(ForkResult::Child) => {
             println!("After my pid is {}, fork() said {}", process::id(), 0);
-        },
+        }
         Err(msg) => {
             eprintln!("Can not create process: {}", msg);
-        },
+        }
     }
 }
