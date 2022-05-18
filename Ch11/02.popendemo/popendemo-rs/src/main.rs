6bc5145dd388d92ca2e4620a18b7a727d2d8620f
SteveLauC <stevelauc@outlook.com>
2022-05-18 19:55:16 +0800

    finish popendemo

--- Ch11/02.popendemo/popendemo-rs/src/main.rs
+++ Ch11/02.popendemo/popendemo-rs/src/main.rs
@@ -1,13 +1,13 @@
 /*
   C's `FILE` is buffered so we use `BufReader` here
 
-  maybe a bug:
-  `FILE *` constructed by `popen()` have to be closed via `pclose()`, but here in rust
-  the read end of pipe is converted to a `BufReader`. I am not sure if this will make
-  child-process a zombie process 
+  And `FILE *` constructed using `popen()` must be closed via `pclose()` for
+  the reason that `wait()` will be called in `pclose()`'s implementation. So
+  I just manually call `wait()` here to avoid zombie process
 */
 
 use libc::{fileno, popen, FILE};
+use nix::sys::wait::wait;
 use std::ffi::CString;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -30,5 +30,7 @@ fn main() {
         .map(|res| res.unwrap())
         .for_each(|line_contents| {
             println!("{}", line_contents);
-        })
+        });
+
+    println!("{:?}", wait()); // manually call `wait()`
 }
