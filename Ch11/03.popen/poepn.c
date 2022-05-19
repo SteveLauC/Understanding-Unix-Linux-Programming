ddc12da194c7e7ba365e63bdc2b8c6ada6862632
SteveLauC <stevelauc@outlook.com>
2022-05-19 15:27:42 +0800

    finish popen

---
+++ Ch11/03.popen/poepn.c
@@ -0,0 +1,103 @@
+/*
+ * popen.c: a version of UNIX popen() libaray function
+ *
+ *
+ * FILE * popen(const char * command, const char * type);
+ *
+ * action: execute `command`
+ *
+ * arguments:
+ *      `command` is a normal shell command
+ *      `type` is either "r" or "w"
+ *
+ * return: a stream attached to the `command`, or NULL
+ *
+ */
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// due to the conclicting type of function signature, I change the name to 
+// `my_popen`
+FILE* my_popen(const char* command, char* type)
+{
+    assert(command != NULL);
+    assert(type != NULL);
+
+    // get a pipe
+    int pipe_fd[2] = {-1, -1};
+    if (pipe(pipe_fd) == -1)
+    {
+        perror("Can not pipe");
+        exit(1);
+    }
+
+    // fork a process
+    int pid = fork();
+    if (pid == -1)
+    {
+        perror("Can not fork");
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        // child-process
+        if (*type == 'r')
+        {
+            close(pipe_fd[0]);
+            // redirect stdout to the pipe write end
+            dup2(pipe_fd[1], 1);
+        }
+        else if (*type == 'w')
+        {
+            close(pipe_fd[1]);
+            // redirect stdin to the pipe read end
+            dup2(pipe_fd[0], 0);
+        }
+        else
+        {
+            fprintf(stderr, "invalid type argument");
+            exit(1);
+        }
+        // execute `command`
+        execl("/usr/bin/bash", "sh", "-c", command, NULL);
+    }
+    else
+    {
+        // parent-process
+        if (*type == 'r')
+        {
+            close(pipe_fd[1]); // close the write end
+            return fdopen(pipe_fd[0], type);
+        }
+        else if (*type == 'w')
+        {
+            close(pipe_fd[0]);
+            return fdopen(pipe_fd[1], type);
+        }
+        else
+        {
+            fprintf(stderr, "invalid type argument");
+            exit(1);
+        }
+    }
+}
+
+// use code from `popendemo.c` to test this
+int main()
+{
+    FILE* fp = NULL;
+    char buf[100] = {'\0'};
+    int i = 0;
+
+    fp = my_popen("who|sort", "r");
+
+    while (fgets(buf, 100, fp) != NULL)
+    {
+        printf("%3d %s", i++, buf);
+    }
+
+    pclose(fp);
+    return 0;
+}
---
+++ Ch11/03.popen/popen-rs/src/main.rs
@@ -0,0 +1,75 @@
+//! In Rust, we don't have `FILE` type, so I am gonna make the signature
+//! something like the following one:
+//!
+//! fn my_popen<S: AsRef<OsStr>>(command: S, op_type: Type) -> BufReader<File>
+//!
+//! To utilize the benefit of pattern matching, I will define a type `Type`
+//! to represent the operation type
+//!
+
+use nix::sys::wait::wait;
+use nix::unistd::{close, dup2, fork, pipe, ForkResult};
+use std::ffi::OsStr;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::os::unix::io::{FromRawFd, RawFd};
+use std::os::unix::prelude::CommandExt;
+use std::process::Command;
+
+#[derive(PartialEq)]
+enum Type {
+    Read,
+    Write,
+}
+
+fn my_popen<S: AsRef<OsStr>>(command: S, op_type: Type) -> BufReader<File> {
+    // get a pipe
+    let (read_end, write_end): (RawFd, RawFd) = pipe().unwrap();
+
+    if let Ok(res) = unsafe { fork() } {
+        match res {
+            ForkResult::Child => {
+                if op_type == Type::Read {
+                    close(read_end).unwrap();
+                    dup2(write_end, 1).unwrap();
+                } else {
+                    close(write_end).unwrap();
+                    dup2(read_end, 0).unwrap();
+                }
+                Command::new("bash")
+                    .args(["-c", command.as_ref().to_str().unwrap()])
+                    .exec();
+            }
+            ForkResult::Parent { child: _ } => {
+                if op_type == Type::Read {
+                    close(write_end).unwrap();
+                    return BufReader::new(unsafe { File::from_raw_fd(read_end) });
+                } else {
+                    close(read_end).unwrap();
+                    return BufReader::new(unsafe { File::from_raw_fd(write_end) });
+                }
+            }
+        }
+    }
+    unimplemented!()
+}
+
+fn my_pclose() {
+    wait().unwrap();
+}
+
+fn main() {
+    let command: &str = "who|sort";
+    let op_type: Type = Type::Read;
+
+    let buffered_file: BufReader<File> = my_popen(command, op_type);
+
+    buffered_file
+        .lines()
+        .map(|res| res.unwrap())
+        .for_each(|line_contents| {
+            println!("{}", line_contents);
+        });
+
+    my_pclose();
+}
