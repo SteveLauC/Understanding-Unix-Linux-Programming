6847483b4753bb0724745f4122aa94169df59a2d
SteveLauC <stevelauc@outlook.com>
2022-05-22 15:56:54 +0800

    finish socklib

---
+++ Ch12/01.socklib/socklib-rs/src/lib.rs
@@ -0,0 +1,9 @@
+//! The api encapsulated in the rust standard library is quite concise and 
+//! straightforward, so there is no need to wrap the libc syscall again.
+//!
+//! If you wanna see what it looks like when using libc syscall, there are
+//! two code snippets:
+//!
+//! 1. [client](https://github.com/SteveLauC/Passionate-TCP-IP-Socket-Programming/blob/main/Ch1/hello_client-rs/src/main.rs)
+//! 2. [server](https://github.com/SteveLauC/Passionate-TCP-IP-Socket-Programming/blob/main/Ch1/hello_server-rs/src/main.rs)
+//!
