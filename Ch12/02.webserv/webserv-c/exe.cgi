cb4d7e685f2a1c055329b50ab0fc6d20a1db8bb5
SteveLauC <stevelauc@outlook.com>
2022-05-24 12:35:36 +0800

    finish webserv-c

---
+++ Ch12/02.webserv/webserv-c/exe.cgi
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+echo "hello world"
+
+ls
+
+echo "this is the output of exe.cgi"
---
+++ Ch12/02.webserv/webserv-c/socklib.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+
+int make_server_socket(uint16_t port_num, int backlog);
+int connect_to_server(char * hostname, uint16_t port_num);
