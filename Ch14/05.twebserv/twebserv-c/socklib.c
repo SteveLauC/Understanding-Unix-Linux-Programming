cc10b4527c886280c3fc6ad843fbfd61fc446984
SteveLauC <stevelauc@outlook.com>
2022-06-04 20:09:03 +0800

    finish twebserv-c

---
+++ Ch14/05.twebserv/twebserv-c/socklib.c
@@ -0,0 +1,91 @@
+/*
+ * socklib.c: This file contains functions used lots when writing internet 
+ *            client/server programs. 
+ *
+ * The two main function here are:
+ *     1. int make_server_socket(int port_num, int backlog);
+ *     return a server socket or -1 if on error
+ *     2. int connect_to_server(char * hostname, int port_num); 
+ *     return a connected socket or -1 if on error
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdint.h>
+
+#define HOSTNAME_LEN 64
+#define BACKLOG 1
+
+int make_server_socket(uint16_t port_num, int backlog);
+int connect_to_server(char * hostname, uint16_t port_num);
+
+
+int make_server_socket(uint16_t port_num, int backlog) {
+    // get a socket
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (-1 == server_fd) {
+        perror("socket");
+        return -1;
+    }
+
+    // bind a address
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    server_address.sin_port = htons(port_num);
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+    char hostname[HOSTNAME_LEN];
+    memset(hostname, 0, HOSTNAME_LEN);
+    gethostname(hostname, HOSTNAME_LEN);
+    struct hostent * hp = gethostbyname(hostname);
+    if (NULL == hp || AF_INET != hp->h_addrtype) {
+        perror("gethostbyname");
+        return -1;
+    }
+    memcpy(&server_address.sin_addr, hp->h_addr_list[0],  hp->h_length);
+    if (bind(server_fd, (struct sockaddr *)&server_address, sizeof(server_address)) != 0) {
+        perror("bind");
+        return -1; 
+    }
+
+    // activate the server
+    if (listen(server_fd, backlog) != 0) {
+        perror("listen");
+        return -1;
+    }
+
+    printf("[webserv]: server started, listening on: %s:%d\n", hostname, port_num);
+
+    return server_fd;
+}
+
+
+int connect_to_server(char * hostname, uint16_t port_num) {
+    // get a socket
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_fd == -1) {
+        return -1;
+    }
+
+    // connect to the server 
+    struct hostent * hp = gethostbyname(hostname);
+    if (NULL == hp || AF_INET != hp->h_addrtype) {
+        return -1;
+    }
+    struct sockaddr_in server_address;
+    memset(&server_address, 0, sizeof(server_address));
+    memcpy(&server_address.sin_addr, hp->h_addr_list[0], hp->h_length);
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port_num);
+    if (connect(client_fd, (struct sockaddr *)&server_address, sizeof(server_address))  != 0) {
+        return -1;
+    }
+
+    return client_fd;
+}
