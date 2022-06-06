200572068013870eb091d50e20b181b49aa440b4
SteveLauC <stevelauc@outlook.com>
2022-06-06 14:37:00 +0800

    finish shm_ts2

---
+++ Ch15/02.shm_ts/shm_ts2-rs/src/main.rs
@@ -0,0 +1,105 @@
+use chrono::{DateTime, Local};
+use libc::{c_short, c_void, shmctl, IPC_CREAT, IPC_EXCL, IPC_RMID};
+use libc::{sembuf, semctl, semget, semop, shmat, shmget, shmid_ds};
+use std::ffi::CString;
+use std::process::exit;
+use std::ptr::null;
+use std::thread::sleep;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+const SEM_UNDO: c_short = 0x1000;
+const SETVAL: i32 = 16;
+
+const TIME_MEM_KEY: i32 = 99;
+const TIME_SEM_KEY: i32 = 9900;
+const SEG_SIZE: usize = 100;
+
+fn main() {
+    let seg_id: i32 = unsafe { shmget(TIME_MEM_KEY, SEG_SIZE, IPC_CREAT | 0o777) };
+    if seg_id == -1 {
+        eprintln!("shmget");
+        exit(1);
+    }
+
+    let mem_ptr: *mut c_void = unsafe { shmat(seg_id, null(), 0) };
+    if mem_ptr == (-1_i32) as *mut c_void {
+        eprintln!("shmat");
+        exit(2);
+    }
+
+    let sem_set_id: i32 = unsafe { semget(TIME_SEM_KEY, 2, IPC_CREAT | IPC_EXCL | 0o666) };
+    if sem_set_id == -1 {
+        eprintln!("semget");
+        exit(3);
+    }
+
+    set_sem_value(sem_set_id, 0, 0);
+    set_sem_value(sem_set_id, 1, 0);
+
+    // run for a minute
+    for _ in 0..60 {
+        let now: CString = CString::new(show_time(
+            SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs(),
+        ))
+        .unwrap();
+        println!("\tshm_ts2 waiting for lock");
+        wait_and_lock(sem_set_id);
+        println!("\tshm_ts2 updating memory");
+        unsafe { mem_ptr.copy_from(now.as_ptr() as *const c_void, now.as_bytes().len() + 1) };
+        release_lock(sem_set_id);
+        println!("\tshm_ts2 released lock");
+        sleep(Duration::from_secs(1));
+    }
+
+    unsafe {
+        shmctl(seg_id, IPC_RMID, null() as *const shmid_ds as *mut shmid_ds);
+        semctl(sem_set_id, 0, IPC_RMID, null::<*const i32>());
+    }
+}
+
+fn show_time(seconds: u64) -> String {
+    let date: DateTime<Local> = DateTime::from(UNIX_EPOCH + Duration::from_secs(seconds));
+    date.format("%b %e %H:%M:%S %Y").to_string()
+}
+
+fn set_sem_value(sem_set_id: i32, sem_num: i32, val: i32) {
+    if unsafe { semctl(sem_set_id, sem_num, SETVAL, val) } == -1 {
+        eprintln!("semctl");
+        exit(4);
+    }
+}
+
+fn wait_and_lock(sem_set_id: i32) {
+    let mut actions: [sembuf; 2] = [
+        sembuf {
+            sem_num: 0,
+            sem_flg: SEM_UNDO,
+            sem_op: 0,
+        },
+        sembuf {
+            sem_num: 1,
+            sem_flg: SEM_UNDO,
+            sem_op: 1,
+        },
+    ];
+    if unsafe { semop(sem_set_id, &mut actions as *mut sembuf, 2) } == -1 {
+        eprintln!("semop: locking");
+        exit(10);
+    }
+}
+
+fn release_lock(sem_set_id: i32) {
+    let mut actions: [sembuf; 1] = [sembuf {
+        sem_num: 1,
+        sem_flg: SEM_UNDO,
+        sem_op: -1,
+    }];
+
+    if unsafe { semop(sem_set_id, &mut actions as *mut sembuf, 1) } == -1 {
+        eprintln!("semop: unlocking");
+        exit(10);
+    }
+}
---
+++ Ch15/02.shm_ts/shm_ts2.c
@@ -0,0 +1,105 @@
+/*
+ * shm_ts2.c: time server shared memory version 2(use semaphores for locking)
+ *
+ * program uses shared memory with key 99
+ * program uses semaphore set with key 9900
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <time.h>
+#include <sys/sem.h>
+#include <signal.h>
+#include <sys/types.h>
+
+
+#define TIME_MEM_KEY 99
+#define TIME_SEM_KEY 9900
+#define SEG_SIZE ((size_t)100)
+#define oops(m, x) {perror(m); exit(x);}
+
+int seg_id;
+int sem_set_id;
+void clean_up(int);
+void set_sem_value(int, int, int);
+void wait_and_lock(int);
+void release_lock(int);
+
+int main(void) {
+    char * mem_ptr = NULL; 
+    time_t now;
+    signal(SIGINT, clean_up);
+
+    seg_id = shmget(TIME_MEM_KEY, SEG_SIZE, IPC_CREAT|0777);
+    if (seg_id == -1) {
+        oops("shmget", 1);
+    }
+
+    mem_ptr = shmat(seg_id, NULL, 0);
+    if (mem_ptr == (void *)-1) {
+        oops("shmat", 2);
+    }
+    
+    sem_set_id = semget(TIME_SEM_KEY, 2, IPC_CREAT|IPC_EXCL|0666);
+    if (sem_set_id == -1) {
+        oops("semget", 3);
+    }
+    // set both to zero
+    semctl(sem_set_id, 0, 0);
+    semctl(sem_set_id, 1, 0);
+
+    // server: run for a minute
+    for(int i = 0; i < 60; i++) {
+        time(&now);
+        printf("\tshm_ts2 waiting for lock\n");
+        wait_and_lock(sem_set_id);
+        printf("\tshm_ts2 updaing memory\n");
+        strcpy(mem_ptr, ctime(&now));
+        // sleep(5);
+        release_lock(sem_set_id);
+        printf("\tshm_ts2 release lock\n");
+        sleep(1);
+    }
+
+    clean_up(0);
+}
+
+void clean_up(int _n) {
+    shmctl(seg_id, IPC_RMID, NULL);
+    semctl(sem_set_id, 0, IPC_RMID, NULL);
+}
+
+void set_sem_value(int sem_set_id, int sem_num, int val) {
+    if (semctl(sem_set_id, sem_num, SETVAL, val) == -1) {
+        oops("semctl", 4);
+    }
+}
+ 
+void wait_and_lock(int sem_set_id) {
+    struct sembuf action[2]; 
+    action[0].sem_num = 0;
+    action[0].sem_flg = SEM_UNDO;
+    action[0].sem_op = 0;
+
+    action[1].sem_num = 1;
+    action[1].sem_flg = SEM_UNDO;
+    action[1].sem_op = +1;
+
+    if (semop(sem_set_id, action, 2) == -1) {
+        oops("semop: locking", 10);
+    }
+}
+
+void release_lock(int sem_set_id) {
+    struct sembuf action[1];
+    action[0].sem_num = 1;
+    action[0].sem_flg = SEM_UNDO;
+    action[0].sem_op = -1;
+
+    if (semop(sem_set_id, action, 1) == -1) {
+        oops("semop: unlocking", 10);
+    }
+}
